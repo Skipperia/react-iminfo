4cf81ba8a358513f89f3cba56fec18e7c1c7ed97
Alex Kipper <kippera27@gmail.com>
2024-03-19 17:41:23 +0200

    removed console log

--- src/renderer.js
+++ src/renderer.js
@@ -29,7 +29,3 @@
 import './index.css';
 import "./index.js";
 
-console.log(
-    '👋 This message is being logged by "renderer.js", included via webpack'
-);
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
